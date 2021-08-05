c421ae8388125a55fe15bcab21ab93247a52f2c1
HC Geum <49156892+Doyosae@users.noreply.github.com>
2021-08-06 04:12:50 +0900

    Update model_utility.py

--- model_utility.py
+++ model_utility.py
@@ -217,3 +217,331 @@ def save_lines(filename, datapath: str):
     for data in filename:
         f.write(data + "\n") 
     f.close()
+    
+
+
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+# SINTEL 데이터셋을 위한 함수
+# def flow_read
+# def depth_read
+# def cam_raed
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+# TAG_FLOAT = 202021.25
+# TAG_CHAR  = 'PIEH'
+# def read_flow(filename):
+#     """
+#     Read optical flow from file, return (U,V) tuple. 
+#     Original code by Deqing Sun, adapted from Daniel Scharstein.
+#     """
+#     f = open(filename,'rb')
+#     check = np.fromfile(f,dtype=np.float32,count=1)[0]
+#     assert check == TAG_FLOAT, ' flow_read:: Wrong tag in flow file (should be: {0}, is: {1}). Big-endian machine? '.format(TAG_FLOAT,check)
+#     width = np.fromfile(f,dtype=np.int32,count=1)[0]
+#     height = np.fromfile(f,dtype=np.int32,count=1)[0]
+#     size = width*height
+#     assert width > 0 and height > 0 and size > 1 and size < 100000000, ' flow_read:: Wrong input size (width = {0}, height = {1}).'.format(width,height)
+#     tmp = np.fromfile(f,dtype=np.float32,count=-1).reshape((height,width*2))
+#     u = tmp[:,np.arange(width)*2]
+#     v = tmp[:,np.arange(width)*2 + 1]
+#     return u,v
+
+
+# def read_depth(filename):
+#     """ 
+#     Read depth data from file, return as numpy array.
+#     """
+#     f = open(filename,'rb')
+#     check = np.fromfile(f,dtype=np.float32,count=1)[0]
+#     assert check == TAG_FLOAT, ' depth_read:: Wrong tag in flow file (should be: {0}, is: {1}). Big-endian machine? '.format(TAG_FLOAT,check)
+#     width = np.fromfile(f,dtype=np.int32,count=1)[0]
+#     height = np.fromfile(f,dtype=np.int32,count=1)[0]
+#     size = width*height
+#     assert width > 0 and height > 0 and size > 1 and size < 100000000, ' depth_read:: Wrong input size (width = {0}, height = {1}).'.format(width,height)
+#     depth = np.fromfile(f,dtype=np.float32,count=-1).reshape((height,width))
+#     return depth
+
+
+# def read_cam(filename):
+#     """
+#     Read camera data, return (M,N) tuple.
+#     M is the intrinsic matrix, N is the extrinsic matrix, so that
+
+#     x = M*N*X,
+#     with x being a point in homogeneous image pixel coordinates, X being a
+#     point in homogeneous world coordinates.
+#     """
+#     f     = open(filename,'rb')
+#     check = np.fromfile(f, dtype = np.float32, count = 1)[0]
+#     assert check == TAG_FLOAT, ' cam_read:: Wrong tag in flow file (should be: {0}, is: {1}). Big-endian machine? '.format(TAG_FLOAT,check)
+#     intrinsic = np.fromfile(f,dtype='float64',count=9).reshape((3,3))
+#     extrinsic = np.fromfile(f,dtype='float64',count=12).reshape((3,4))
+#     return intrinsic, extrinsic
+
+
+
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+# FLYINGTHINGS3D 데이터셋을 위한 함수
+# def read
+# def write
+# def readPFM
+# ...
+# ...
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+# def read(file):
+#     if file.endswith('.float3'): return readFloat(file)
+#     elif file.endswith('.flo'): return readFlow(file)
+#     elif file.endswith('.ppm'): return readImage(file)
+#     elif file.endswith('.pgm'): return readImage(file)
+#     elif file.endswith('.png'): return readImage(file)
+#     elif file.endswith('.jpg'): return readImage(file)
+#     elif file.endswith('.pfm'): return readPFM(file)[0]
+#     else: raise Exception('don\'t know how to read %s' % file)
+
+
+# def write(file, data):
+#     if file.endswith('.float3'): return writeFloat(file, data)
+#     elif file.endswith('.flo'): return writeFlow(file, data)
+#     elif file.endswith('.ppm'): return writeImage(file, data)
+#     elif file.endswith('.pgm'): return writeImage(file, data)
+#     elif file.endswith('.png'): return writeImage(file, data)
+#     elif file.endswith('.jpg'): return writeImage(file, data)
+#     elif file.endswith('.pfm'): return writePFM(file, data)
+#     else: raise Exception('don\'t know how to write %s' % file)
+
+
+# def readPFM(file):
+#     file = open(file, 'rb')
+#     color = None
+#     width = None
+#     height = None
+#     scale = None
+#     endian = None
+
+#     header = file.readline().rstrip()
+#     if header.decode("ascii") == 'PF':
+#         color = True
+#     elif header.decode("ascii") == 'Pf':
+#         color = False
+#     else:
+#         raise Exception('Not a PFM file.')
+
+#     dim_match = re.match(r'^(\d+)\s(\d+)\s$', file.readline().decode("ascii"))
+#     if dim_match:
+#         width, height = list(map(int, dim_match.groups()))
+#     else:
+#         raise Exception('Malformed PFM header.')
+
+#     scale = float(file.readline().decode("ascii").rstrip())
+#     if scale < 0: # little-endian
+#         endian = '<'
+#         scale = -scale
+#     else:
+#         endian = '>' # big-endian
+
+#     data = np.fromfile(file, endian + 'f')
+#     shape = (height, width, 3) if color else (height, width)
+
+#     data = np.reshape(data, shape)
+#     data = np.flipud(data)
+#     return data, scale
+
+
+# def writePFM(file, image, scale=1):
+#     file = open(file, 'wb')
+#     color = None
+
+#     if image.dtype.name != 'float32':
+#         raise Exception('Image dtype must be float32.')
+
+#     image = np.flipud(image)
+#     if len(image.shape) == 3 and image.shape[2] == 3: # color image
+#         color = True
+#     elif len(image.shape) == 2 or len(image.shape) == 3 and image.shape[2] == 1: # greyscale
+#         color = False
+#     else:
+#         raise Exception('Image must have H x W x 3, H x W x 1 or H x W dimensions.')
+
+#     file.write('PF\n' if color else 'Pf\n'.encode())
+#     file.write('%d %d\n'.encode() % (image.shape[1], image.shape[0]))
+#     endian = image.dtype.byteorder
+#     if endian == '<' or endian == '=' and sys.byteorder == 'little':
+#         scale = -scale
+
+#     file.write('%f\n'.encode() % scale)
+#     image.tofile(file)
+
+
+# def readFlow(name):
+#     if name.endswith('.pfm') or name.endswith('.PFM'):
+#         return readPFM(name)[0][:,:,0:2]
+#     f = open(name, 'rb')
+
+#     header = f.read(4)
+#     if header.decode("utf-8") != 'PIEH':
+#         raise Exception('Flow file header does not contain PIEH')
+
+#     width = np.fromfile(f, np.int32, 1).squeeze()
+#     height = np.fromfile(f, np.int32, 1).squeeze()
+#     flow = np.fromfile(f, np.float32, width * height * 2).reshape((height, width, 2))
+#     return flow.astype(np.float32)
+
+
+# def readImage(name):
+#     if name.endswith('.pfm') or name.endswith('.PFM'):
+#         data = readPFM(name)[0]
+#         if len(data.shape)==3:
+#             return data[:,:,0:3]
+#         else:
+#             return data
+#     return misc.imread(name)
+
+
+# def writeImage(name, data):
+#     if name.endswith('.pfm') or name.endswith('.PFM'):
+#         return writePFM(name, data, 1)
+#     return misc.imsave(name, data)
+
+
+# def writeFlow(name, flow):
+#     f = open(name, 'wb')
+#     f.write('PIEH'.encode('utf-8'))
+#     np.array([flow.shape[1], flow.shape[0]], dtype=np.int32).tofile(f)
+#     flow = flow.astype(np.float32)
+#     flow.tofile(f)
+
+
+# def readFloat(name):
+#     f = open(name, 'rb')
+#     if(f.readline().decode("utf-8"))  != 'float\n':
+#         raise Exception('float file %s did not contain <float> keyword' % name)
+
+#     dim = int(f.readline())
+#     dims = []
+#     count = 1
+#     for i in range(0, dim):
+#         d = int(f.readline())
+#         dims.append(d)
+#         count *= d
+
+#     dims = list(reversed(dims))
+#     data = np.fromfile(f, np.float32, count).reshape(dims)
+#     if dim > 2:
+#         data = np.transpose(data, (2, 1, 0))
+#         data = np.transpose(data, (1, 0, 2))
+#     return data
+
+
+# def writeFloat(name, data):
+#     f = open(name, 'wb')
+#     dim=len(data.shape)
+#     if dim>3:
+#         raise Exception('bad float file dimension: %d' % dim)
+
+#     f.write(('float\n').encode('ascii'))
+#     f.write(('%d\n' % dim).encode('ascii'))
+
+#     if dim == 1:
+#         f.write(('%d\n' % data.shape[0]).encode('ascii'))
+#     else:
+#         f.write(('%d\n' % data.shape[1]).encode('ascii'))
+#         f.write(('%d\n' % data.shape[0]).encode('ascii'))
+#         for i in range(2, dim):
+#             f.write(('%d\n' % data.shape[i]).encode('ascii'))
+
+#     data = data.astype(np.float32)
+#     if dim==2:
+#         data.tofile(f)
+
+#     else:
+#         np.transpose(data, (2, 0, 1)).tofile(f)
+
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+기타 모듈
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+def tensor2numpy(tensor): # 토치 텐서를 넘파이로
+    return tensor.numpy()
+
+
+def numpy2tensor(numpy): # 넘파이를 토치 텐서로
+    return torch.from_numpy(numpy)
+
+
+def parameter_number(model): # 모델의 파라미터를 계산하는 함수
+    num_params = 0
+    for tensor in list(model.parameters()):
+        tensor      = tensor.view(-1)
+        num_params += len(tensor)
+    print(num_params)
+
+
+def sample_dataset(dataloader): # 모델 데이터로터에서 배치 샘플 하나를 추출
+    test = 0
+    start = time.time()
+    for index, data in enumerate(dataloader):
+        test = data
+        if index == 0:
+            break  
+    print("batch sampling time:  ", time.time() - start)
+    return test
+
+
+def show_image(image, option = "torch", size = (10, 4), cmap = "magma", show_disp = True):
+    """
+    토치나 텐서플로우 형태의 이미지를 받아서 이미지를 띄우는 함수
+    Args: tensor type
+            Pytorch:    [B, N, H, W]
+            Tensorflow: [B, H, W, C]
+    """
+    plt.rcParams["figure.figsize"] = size
+
+    if option == "torch":
+        if image.shape[0] == 3 or len(image.shape) == 3:
+            image = np.transpose(image, (1, 2, 0))
+        else:
+            image = np.squeeze(image, axis = 0)
+            image = np.transpose(image, (1, 2, 0))
+
+    elif option == "tensorflow": # N H W C
+        if len(image.shape) == 3:
+            pass
+        else:
+            image = np.squeeze(image, axis = 3)
+
+    """
+    uint8 -> float32로 바꾸면 엄청 큰 정수 단위의 float32가 됨
+    따리서 255.로 나누어 주는 것이 중요
+    그리고 cv2.imread로 불러온 이미지를 plt.imshow로 띄울때는 cv2.COLOR_BGR2RGB
+    """
+    if show_disp:
+        plt.imshow(image, cmap = cmap, vmax = np.percentile(image, 95))
+        plt.axis('off')
+        plt.show()
+    else:
+        plt.imshow(image, cmap = cmap)
+        plt.axis('off')
+        plt.show()
+
+
+def one_graph(data, xlabel, ylabel, title, color, marker, linestyle):
+    """
+    data: np.array
+    xlabel: str
+    ylabel: str
+    title: str
+    color: str
+    marker: "o" or 
+    """
+    plt.rcParams["figure.figsize"] = (10, 6)
+    plt.rcParams['lines.linewidth'] = 1.5
+    plt.rcParams['axes.grid'] = True 
+    plt.rc('font', size = 10)        # 기본 폰트 크기
+    plt.rc('axes', labelsize = 15)   # x,y축 label 폰트 크기
+    plt.rc('figure', titlesize = 15) # figure title 폰트 크기
+
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.plot(data, c = color, marker = marker, linestyle = linestyle)
+    plt.show()
